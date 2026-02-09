696a26c0dc8c81591c1a3a03634e9f8bcdf99176
aecao <aec332@cornell.edu>
2026-02-09 08:45:52 -0500

    chore: remove start/place buttons

--- src/next-button.js
+++ src/next-button.js
@@ -1,6 +1,5 @@
 import {modelDescriptions, getSelectedIndex, setSelectedIndex} from './data.js'
 import {openPopup} from './popup.js'
-import {selectComponent} from './selectcomponent.js'
 import {updateModelVisibility} from './selectcomponent.js'
 
 // Safely update button visibility
@@ -15,49 +14,7 @@ const updateButtonVisibility = () => {
 
 const nextButtonComponent = () => ({
   init() {
-    let startButtonClicked = false
-
-    const startButton = document.getElementById('startbutton')
-    const placeButton = document.getElementById('placebutton')
-    const message = document.getElementById('message')
-
-    if (message) message.style.display = 'block'
-    if (startButton) startButton.style.display = 'block'
-
-    const enableExtraFeatures = () => {
-      const modelElements = document.querySelectorAll('.cantap')
-      const group = document.getElementById('group')
-
-      if (!group) return
-      modelElements.forEach((modelElement) => {
-        const clonedModel = modelElement.cloneNode(true)
-        group.appendChild(clonedModel)
-        modelElement.remove()
-      })
-      group.setAttribute('xrextras-hold-drag', 'rise-height: 0')
-      group.setAttribute('xrextras-two-finger-rotate', '')
-    }
-
-    if (startButton) {
-      startButton.onclick = () => {
-        if (!startButtonClicked) {
-          startButtonClicked = true
-          startButton.style.display = 'none'
-          if (placeButton) placeButton.style.display = 'block'
-          if (message) message.style.display = 'none'
-          enableExtraFeatures()
-        }
-      }
-    }
-
-    if (placeButton) {
-      placeButton.onclick = () => {
-        placeButton.style.display = 'none'
-        setSelectedIndex(0)
-      selectComponent.init?.()
-      updateButtonVisibility()
-      }
-    }
+    // Start/place buttons removed; navigation works without gating
 
     const bindNavButtons = () => {
       const nextButton = document.getElementById('nextbutton')
